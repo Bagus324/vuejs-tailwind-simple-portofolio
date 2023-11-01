f69669d2bee549134d2a177564dadd854dc6966b
Bagus324 <bagusdarmawan654@gmail.com>
2023-11-01 14:59:04 +0700

    init

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
+        <div class="text-center">
+            <a href="https://www.linkedin.com/in/bagus-tri-yulianto-darmawan-8923a8232/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-linkedin" /></a>
+            <a href="#" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-instagram" /></a>
+            <a href="https://github.com/Bagus324" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-brands fa-github" /></a>
+            <a href="mailto:bagusdarmawan654@gmail.com"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-solid fa-envelope" /></a>
+        </div>
+    </div>
+</template>
+<script setup></script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! I'm Bagus Tri Yulianto Darmawan </h2>
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+            I'm an undergraduate student at the Politeknik Negeri Jakarta, in the field of Machine Learning NLP. I'm currently expanding
+            my horizon learning Data Science and Web Development at the same time and I just got my Associated Data Engineer Certification few days ago. 
+            Currently doing internship for Pertamedika IHC at RSPP. Outside of my Work/College life, I like playing DotA 2 and Apex Legend, also watch some anime.
+        </p>
+        <!-- <p class="text-center text-blue-600">#DotNetCore #WebApplication</p> -->
+    </div>
+</template>
+<script setup>
+
+
+</script>
